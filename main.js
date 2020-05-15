cd9753fcbbca7d35d0d8dd059e6869726df35733
Otávio Barreto de Oliveira Silva <otaviosilva2632@gmail.com>
2020-05-14 22:42:42 -0300

    Restyling

--- main.js
+++ main.js
@@ -1,59 +1,85 @@
-setTimeout(function () {
-  document.getElementById("loading").classList.add("animated");
-  document.getElementById("loading").classList.add("fadeOut");
-  setTimeout(function () {
-    document.getElementById("loading").classList.remove("animated");
-    document.getElementById("loading").classList.remove("fadeOut");
-    document.getElementById("loading").style.display = "none";
-  }, 800);
-}, 1500);
-$.getJSON("blog.json", function (blog) {
-  blog = blog || [];
-  if (blog.length == 0) {
-    return (document.getElementById("blog_section").style.display =
-      "none");
-  }
-  for (var i = 0; i < blog.length; i++) {
-    $("#blogs").append(`
-            <a href="./blog/${blog[i].url_title}/" target="_blank">
-                <section>
-                    <img src="./blog/${blog[i].url_title}/${blog[i].top_image}">
-                    <div class="blog_container">
-                        <div class="section_title">${blog[i].title}</div>
-                        <div class="about_section">
-                            ${blog[i].sub_title}
-                        </div>
-                    </div>
-                </section>
-            </a>
-            `);
-  }
-}).fail(function () {
-  return (document.getElementById("blog_section").style.display = "none");
-});
+$.getJSON("data/repos.json", repos => {
+  repos.forEach(repo => {
+    if (!repo.fork) {
+      console.log(`
+          <a href="${repo.html_url}" target="_blank">
+            <section>
+              <div class="section_title">${repo.name}</div>
+              <div class="about_section">
+                <span style="display:block">${repo.description}</span>
+              </div>
+              <div class="bottom_section">
+                <span style="display:inline-block"><i class="fas fa-code"></i> ${repo.language}</span>
+                <span><i class="fas fa-star"></i> ${repo.stargazers_count}</span>
+                <span><i class="fas fa-code-branch"></i> ${repo.forks}</span>
+              </div>
+            </section>
+          </a>
+        `)
+    } 
+    // else {
+    //   $('.forks').append(`
+    //       <a href="${repo.html_url}" target="_blank">
+    //         <section>
+    //           <div class="section_title">${repo.name}</div>
+    //           <div class="about_section">
+    //             <span style="display:block">${repo.description}</span>
+    //           </div>
+    //           <div class="bottom_section">
+    //             <span style="display:inline-block">
+    //               <i class="fas fa-code"></i> 
+    //               ${repo.language}
+    //             </span>
+    //             <span>
+    //               <i class="fas fa-star"></i> 
+    //               ${repo.stargazers_count}
+    //             </span>
+    //             <span>
+    //               <i class="fas fa-code-branch"></i> 
+    //               ${repo.forks}
+    //             </span>
+    //           </div>
+    //         </section>
+    //       </a>
+    //     `)
+    // }
+  })
 
+}).fail(() => {
+  return 
+})
+
+setTimeout(function () {
+  document.getElementById("loading").classList.add("animated")
+  document.getElementById("loading").classList.add("fadeOut")
+  setTimeout(function () {
+    document.getElementById("loading").classList.remove("animated")
+    document.getElementById("loading").classList.remove("fadeOut")
+    document.getElementById("loading").style.display = "none"
+  }, 800)
+}, 500)
 
 const magicProjectsGrid = new MagicGrid({
-  container: "#work_section",
+  container: ".work_section",
   animate: false,
-  gutter: 30, // default gutter size
+  gutter: 30,
   static: true,
   useMin: false,
   maxColumns: 2,
   useTransform: true
-});
+})
 
 const magicForksGrid = new MagicGrid({
-  container: "#forks_section",
+  container: ".forks_section",
   animate: false,
-  gutter: 30, // default gutter size
+  gutter: 30,
   static: true,
   useMin: false,
   maxColumns: 2,
   useTransform: true
-});
+})
 
 $("document").ready(() => {
-  magicProjectsGrid.listen();
-  magicForksGrid.listen();
-});
+  magicProjectsGrid.listen()
+  // magicForksGrid.listen()
+})
