e70f78484e0c061cb2d47f4a1f8f9f472dd475f1
Otavio Barreto de Oliveira Silva <otaviosilva2632@gmail.com>
2020-06-20 22:14:27 -0300

    Update data

--- main.js
+++ main.js
@@ -1,34 +1,29 @@
-$.getJSON("data/repos.json", repos => {
-  repos.forEach(repo => {
-    if (!repo.fork) {
-      console.log(`
-        <a href="${repo.html_url}" target="_blank">
-          <section>
-            <div class="section_title">${repo.name}</div>
-            <div class="about_section">
-              <span class="dblock">${repo.description}</span>
-            </div>
-            <div class="bottom_section">
-              <span class="dinblock"><i class="fas fa-code"></i> ${repo.language}</span>
-              <span><i class="fas fa-star"></i> ${repo.stargazers_count}</span>
-              <span><i class="fas fa-code-branch"></i> ${repo.forks}</span>
-            </div>
-          </section>
-        </a>
-      `)
-    } 
-  })
+const workContainer = document.querySelector("#work_section")
 
-}).fail(() => {
-  return 
-})
+setTimeout(function () {
+  $("#loading").addClass("animated");
+  $("#loading").addClass("fadeOut");
+  setTimeout(function () {
+    $("#loading").removeClass("animated");
+    $("#loading").removeClass("fadeOut");
+    $("#loading").css("display", "none");
+  }, 800);
+}, 1500);
 
-// setTimeout(function () {
-//   document.getElementById("loading").classList.add("animated")
-//   document.getElementById("loading").classList.add("fadeOut")
-//   setTimeout(function () {
-//     document.getElementById("loading").classList.remove("animated")
-//     document.getElementById("loading").classList.remove("fadeOut")
-//     document.getElementById("loading").style.display = "none"
-//   }, 800)
-// }, 500)
+
+const magicProjectsGrid = new MagicGrid({
+  container: "#work_section",
+  animate: false,
+  gutter: 30, // default gutter size
+  static: true,
+  useMin: false,
+  maxColumns: 2,
+  useTransform: true
+});
+
+
+$("document").ready(() => {
+  magicProjectsGrid.listen();
+});
+
+
