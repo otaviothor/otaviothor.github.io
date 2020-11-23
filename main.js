eeef9f522296ba2f077676ec34b64d56a085fcef
Otávio <otavio@houpa.app>
2020-11-23 11:10:48 -0300

    feat: listing repos based in github api

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const workContainer = document.querySelector("#work_section")
+const url = `https://api.github.com/users/otaviothor/repos`;
 
 setTimeout(function () {
   $("#loading").addClass("animated");
@@ -8,22 +8,51 @@ setTimeout(function () {
     $("#loading").removeClass("fadeOut");
     $("#loading").css("display", "none");
   }, 800);
-}, 1500);
+}, 500);
 
+const getRepos = () => fetch(url).then((res) => res.json());
 
-const magicProjectsGrid = new MagicGrid({
-  container: "#work_section",
-  animate: false,
-  gutter: 30, // default gutter size
-  static: true,
-  useMin: false,
-  maxColumns: 2,
-  useTransform: true
-});
+const generateHTML = (repos) => repos.reduce(
+    (
+      accumulator,
+      { html_url, name, description, language, stargazers_count, forks }
+    ) => {
+      accumulator += `
+        <a href="${html_url ?? ""}">
+          <section>
+            <div class="section_title">${name ?? ""}</div>
+            <div class="about_section">
+              <span>${description ?? ""}</span>
+            </div>
+            <div class="bottom_section">
+              <span><i class="fas fa-code"></i>${language ?? ""}</span>
+              <span><i class="fas fa-star"></i>${stargazers_count ?? ""}</span>
+              <span><i class="fas fa-code-branch"></i>${forks ?? ""}</span>
+            </div>
+          </section>
+        </a>
+      `;
 
+      return accumulator
+    },
+    ""
+  )
 
-$("document").ready(() => {
-  magicProjectsGrid.listen();
-});
+const insertReposIntoPage = repos => {
+  $("#work_section").html(repos);
+  setTimeout(function () {
+    $("#loading").addClass("animated");
+    $("#loading").addClass("fadeOut");
+    setTimeout(function () {
+      $("#loading").removeClass("animated");
+      $("#loading").removeClass("fadeOut");
+      $("#loading").css("display", "none");
+    }, 800);
+  }, 500);
+}
 
+const reposPromise = getRepos();
 
+reposPromise
+  .then(generateHTML)
+  .then(insertReposIntoPage);
