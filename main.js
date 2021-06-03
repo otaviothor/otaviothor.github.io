c78f66c002e60c507a1b094e8161946726adc390
thor <otaviosilva2632@gmail.com>
2021-06-03 10:36:01 -0300

    feat: refactoring card component

--- main.js
+++ main.js
@@ -3,27 +3,49 @@ Vue.component("repo-card", {
   template: `
     <a href="{{ repo.html_url }}">
       <section>
-        <div class="section_title">{{ repo.name }}</div>
+        <div class="header">
+          <h2>{{ repo.name }}</h2>
+          <div class="info">
+            <span><i class="fas fa-code"></i>{{ repo.language }}</span>
+            <span><i class="fas fa-star"></i>{{ repo.stargazers_count }}</span>
+          </div>
+        </div>
         <div class="about_section">
           <span>{{ repo.description }}</span>
-        </div>
-        <div class="bottom_section">
-          <span><i class="fas fa-code"></i>{{ repo.language }}</span>
-          <span><i class="fas fa-star"></i>{{ repo.stargazers_count }}</span>
         </div>
       </section>
     </a>
   `,
 });
 
-const app = new Vue({
-  el: "#app",
+const profile = new Vue({
+  el: "#profile",
   data: {
+    user: {},
+    user: "otaviothor",
+  },
+  methods: {
+    getUser() {
+      fetch(`https://api.github.com/users/${this.user}`)
+        .then((res) => res.json())
+        .then((data) => (this.user = data));
+    },
+  },
+  beforeMount() {
+    this.getUser();
+  },
+});
+
+const work = new Vue({
+  el: "#work",
+  data: {
+    user: {},
     repos: [],
+    user: "otaviothor",
   },
   methods: {
     getRepos() {
-      fetch("https://api.github.com/users/otaviothor/repos")
+      fetch(`https://api.github.com/users/${this.user}/repos`)
         .then((res) => res.json())
         .then((data) => (this.repos = data));
     },
