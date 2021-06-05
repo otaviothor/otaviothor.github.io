b9bb6491be4c285dc74ee30ec2ee4c91f13bda6d
thor <otaviosilva2632@gmail.com>
2021-06-05 19:03:36 -0300

    fix: solving problem to not render repos with vue component

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
+Vue.component("repo-item", {
+  props: ["repo"],
+  template: `
+    <li class="list-item">
+      <a href="{{ repo.html_url }}">{{ repo.name }}</a>
+      <span>{{ repo.description }}</span>
+      <div>
+        <span><i class="fas fa-code"></i>{{ repo.language }}</span>
+        <span><i class="fas fa-star"></i>{{ repo.stargazers_count }}</span>
+      </div>
+    </li>
+  `,
+});
+
 const profile = new Vue({
   el: "#profile",
   data: {
     user: {},
-    repos: [],
     user: "otaviothor",
   },
   methods: {
@@ -11,6 +24,19 @@ const profile = new Vue({
         .then((res) => res.json())
         .then((data) => (this.user = data));
     },
+  },
+  beforeMount() {
+    this.getUser();
+  },
+});
+
+const repos = new Vue({
+  el: "#repos",
+  data: {
+    repos: [],
+    user: "otaviothor",
+  },
+  methods: {
     getRepos() {
       fetch(
         `https://api.github.com/users/${this.user}/repos?per_page=100&sort=created`
@@ -20,7 +46,6 @@ const profile = new Vue({
     },
   },
   beforeMount() {
-    this.getUser();
     this.getRepos();
   },
 });
